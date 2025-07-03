d1400222f2a72732979b4396eee55c32bea5ad72
조경주 <jkj081677@naver.com>
2025-07-03 22:47:50 +0900

    ><

--- app/src/main/java/com/talkgrow_/util/HandLandmarkerHelper.kt
+++ app/src/main/java/com/talkgrow_/util/HandLandmarkerHelper.kt
@@ -362,7 +362,7 @@ class HandLandmarkerHelper(
         const val DEFAULT_HAND_DETECTION_CONFIDENCE = 0.5F
         const val DEFAULT_HAND_TRACKING_CONFIDENCE = 0.5F
         const val DEFAULT_HAND_PRESENCE_CONFIDENCE = 0.5F
-        const val DEFAULT_NUM_HANDS = 1
+        const val DEFAULT_NUM_HANDS = 2
         const val OTHER_ERROR = 0
         const val GPU_ERROR = 1
     }
